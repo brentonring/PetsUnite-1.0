27ec7031cecc692fa1154607a4205192b5b8ff45
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-05-26 14:04:25 -0400

    changed adoption show page comment emojis

--- views/adoption/show_adoption.jsx
+++ views/adoption/show_adoption.jsx
@@ -11,7 +11,7 @@ function showPet(data) {
         comments = data.pets.comments.map(c => {
             return (
                 <div className='border'>
-                    <h2 className='adopt'>{c.adopt ? 'Interested in adopting! <img id="icons" src="/images/icons/icon_gender32.png"></img> ' : ' ❤️ ' }</h2>
+                    <h2 className='adopt'>{c.adopt ? `I'm interested in adopting! 🥰 `  : 'Admirer! ❤️ ' }</h2>
                     <h4>{c.content}</h4>
                     <h3><strong>- {c.author}</strong></h3>
                     <h4>Rating: {c.stars}</h4>
@@ -52,8 +52,7 @@ function showPet(data) {
                 <button type="submit" className="btn btn-success ">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" style={{ fill: 'white' }}  className="bi bi-pencil-fill" viewBox="0 0 16 16">
                     <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-                    </svg>
-                </button>Edit</a>
+                    </svg>Edit</button></a>                
                 <form method='POST' action={`/adoption/${data.id}?_method=DELETE`}>
                     <button type='submit' className='btn btn-danger  mx-2 my-1'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" style={{ fill: 'white' }} className="bi bi-trash3-fill" viewBox="0 0 16 16">
