8c473b1071875b475801834350ae899207c4c11d
Katie Eells <katieeells@Katies-MBP.lan>
2022-05-29 15:21:06 -0400

    css styling

--- views/adoption/show_adoption.jsx
+++ views/adoption/show_adoption.jsx
@@ -22,31 +22,31 @@ function showPet(data) {
     return (
         <Def>
             <main>
-                <h1>I'M READY FOR MY NEW FAMILY!</h1>                
-                <img src={data.pets.pic} />
+                <h1>I'm ready for my new home!</h1>                
+                    <img src={data.pets.pic} />
                 <h2>Hi! My Name Is {data.pets.name}</h2>
                 <h3>I'm Available</h3>                
-                <li className='list-group-item text-capitalize rounded'>
-                    <img id="icons" src="/images/icons/icon_bread32.png"></img>{data.pets.breed}
-                </li>
-                <li className='list-group-item text-capitalize rounded'>
-                    <img id="icons" src="/images/icons/icon_gender32.png"></img>{data.pets.gender}
-                </li>
-                <li className='list-group-item text-capitalize rounded'>
-                    <img id="icons" src="/images/icons/icon_bday32.png"></img>{data.pets.age} {data.pets.age_unit}
-                </li>
-                <li className='list-group-item text-capitalize rounded'>
-                    <img id="icons" src="/images/icons/icon_heartbeat32.png"></img><span>Health History: {data.pets.health_history}</span>
-                </li>
+                    <li className='list-group-item text-capitalize rounded'>
+                        <img id="icons" src="/images/icons/icon_bread32.png"></img>{data.pets.breed}
+                    </li>
+                    <li className='list-group-item text-capitalize rounded'>
+                        <img id="icons" src="/images/icons/icon_gender32.png"></img>{data.pets.gender}
+                    </li>
+                    <li className='list-group-item text-capitalize rounded'>
+                        <img id="icons" src="/images/icons/icon_bday32.png"></img>{data.pets.age} {data.pets.age_unit}
+                    </li>
+                    <li className='list-group-item text-capitalize rounded'>
+                        <img id="icons" src="/images/icons/icon_heartbeat32.png"></img><span>Health History: {data.pets.health_history}</span>
+                    </li>
                 <a href={`/adoption/${data.pets.id}/edit`} className='btn btn-warning'>Edit</a>
                 <form method='POST' action={`/adoption/${data.pets.id}?_method=DELETE`}>
                     <button type='submit' className='btn btn-danger'>Delete</button>
                 </form>                
                 <h2>Comments</h2>
-                <div className="row">               
-                    {comments}               
-                </div>                
-                <hr />
+                    <div className="row">               
+                        {comments}               
+                    </div>                
+                    <hr />
                 <h2>Please leave a comment!</h2>
                     <form action={`/adoption/${data.pets._id}/comment`} method="POST">
                         <div className="row">
@@ -71,7 +71,7 @@ function showPet(data) {
                         </div>
                         <div className='text-center'>
                         <input className='btn btn-primary' type='submit' value='Add Comment' />
-                    </div>  
+                        </div>  
                     </form>
             </main>
         </Def>
--- views/home.jsx
+++ views/home.jsx
@@ -23,12 +23,10 @@ function Home (){
                 <div>
                     <div id="home-content">
                         <h2>Welcome to PetsUnite!</h2>
-                        <p>A social media space to socialize and explore different pets to adopt, meet other pet anothers at events,
-                            and connect to services for you precious pets. Add, edit and explore the differet options at PetsUnite!
-                        </p>
-                    </div>
-                    <div>
-
+                            <p>A space to socialize and explore. Find animals looking for a new home. Discover different events, classes and 
+                                meet-ups for you and your pet. Connect to a variety of services for our lovable companions. Contribute to this incredible community of 
+                                PetsUnite by posting, commenting and sharing with other fellow pet lovers!
+                            </p>
                     </div>
                 </div>
             </main>
