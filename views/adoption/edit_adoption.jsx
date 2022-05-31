0e4abdce24f118e027d51deb8894bacbefc54b49
Brenton Ring <brentonring@Brentons-Air.lan>
2022-05-31 10:19:09 -0400

    all new and edit pages functional

--- views/adoption/edit_adoption.jsx
+++ views/adoption/edit_adoption.jsx
@@ -8,57 +8,76 @@ function editPet ({pet}) {
                 <h1 style={{textAlign: 'center'}}>Edit this Pet for Adoption</h1>
                 <div>
                     <form method='POST' action={`/adoption/${pet._id}?_method=PUT`}>
-                        <div className='form-group col-sm-6'>
-                            <label htmlFor='name'>Pet Name</label>
+                        <div className='form-group'>
+                            <label className= 'col-form-label' htmlFor='name'>Pet Name</label>
                             <input className='form-control' required id='name' name='name' defaultValue={pet.name}/>
                         </div>
-                        <div className='form-group col-sm-6'>
-                            <label htmlFor='animal_type'>Type of Pet</label>
-                            <select className='form-select' id='animal_type' name='animal_type' defaultValue={pet.animal_type}>
-                                {/* <option></option> */}
-                                <option>Dog</option>
-                                <option>Cat</option>
-                                <option>Bird</option> 
-                                <option>Rodent</option>
-                                <option>Reptile</option>
-                                <option>Fish</option>
-                                <option>Other</option>
-                            </select>
+                        <div className='form-group'>
+                            <label className='col-form-label' htmlFor='animal_type'>Type of Pet</label>
+                                <select className='form-select' id='animal_type' name='animal_type' defaultValue={pet.animal_type}>
+                                    <option>Dog</option>
+                                    <option>Cat</option>
+                                    <option>Bird</option> 
+                                    <option>Rodent</option>
+                                    <option>Reptile</option>
+                                    <option>Fish</option>
+                                    <option>Other</option>
+                                </select>
                         </div>
-                        <div className='form-group col-sm-4'>
-                            <label htmlFor='breed'>Breed</label>
+                        <div className='form-group'>
+                            <label className='col-form-label' htmlFor='breed'>Breed</label>
                             <input className='form-control' id='breed' name='breed' defaultValue={pet.breed}/>
                         </div>
-                        <div className='form-group col-sm-4'>
-                            <label htmlFor='gender'>Gender</label>
-                            <select className='form-select' id='gender' name='gender' defaultValue={pet.gender}>
-                                <option>Unknown</option>
-                                <option>Male</option>
-                                <option>Female</option>
-                            </select>
+                        <div className='form-group'>
+                            <label className='col-form-label' htmlFor='gender'>Gender</label>
+                                <select className='form-select' id='gender' name='gender' defaultValue={pet.gender}>
+                                    <option>Male</option>
+                                    <option>Female</option>
+                                </select>
                         </div>
-                        <div className='form-group col-sm-4'>
-                            <label htmlFor='color'>Color</label>
+                        {/* <fieldset className="form-group">
+                            <div className="row">
+                                <legend className="col-form-label pt-0">Gender</legend>
+                                <div className="col-sm-10">
+                                    <div className="form-check form-check-inline">
+                                        <input className="form-check-input" type="radio" name="gender" id="gender" value="male" />
+                                        <label className="form-check-label" for="gender">
+                                            Male
+                                        </label>
+                                    </div>
+                                    <div className="form-check form-check-inline">
+                                        <input className="form-check-input" type="radio" name="gender" id="gender" value="female" />
+                                        <label className="form-check-label" for="gender">
+                                            Female
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </fieldset> */}
+                        <div className='form-group'>
+                            <label className='col-form-label' htmlFor='color'>Color</label>
                             <input className='form-control' id='color' name='color' defaultValue={pet.color}/>
                         </div>
-                        <div className='form-group col-sm-3'>
-                            <label htmlFor='age'>Age</label>
-                            <input className='form-control' type='number' id='age' name='age' defaultValue={pet.age}/>
-                        </div>
-                        <div className='form-group col-sm-3'>
-                            <label htmlFor='age_unit'>Unit</label>
+                        <div className='form-group'>
+                            <label className='col-form-label' htmlFor='age'>Age</label>
+                            <input className='form-control' type='number' id='age' name='age' min='1' defaultValue={pet.age} />
+                            <label className='col-form-label' htmlFor='age_unit'></label>
                             <select className='form-select' id='age_unit' name='age_unit' defaultValue={pet.age_unit}>
                                 <option value='years'>Years</option>
                                 <option value='months'>Months</option>
                                 <option value='weeks'>Weeks</option>
                             </select>
                         </div>
-                        <div className='form-group col-sm-3'>
-                            <label htmlFor='health_history'>Health History</label>
-                            <input className='form-control' id='health_history' name='health_history' defaultValue={pet.health_history}/>
+                        <div className='form-group'>
+                            <label className='col-form-label' htmlFor='bio'>Bio</label>
+                            <textarea className='form-control' id='bio' name='bio' defaultValue={pet.bio}/>
                         </div>
-                        <div className='form-group col-sm-3'>
-                            <label htmlFor='pic'>Picture</label>
+                        <div className='form-group'>
+                            <label className='col-form-label' htmlFor='health_history'>Health History</label>
+                            <textarea className='form-control' id='health_history' name='health_history' defaultValue={pet.health_history}/>
+                        </div>
+                        <div className='form-group'>
+                            <label className='col-form-label' htmlFor='pic'>Picture</label>
                             <input className='form-control' id='pic' name='pic' defaultValue={pet.pic}/>
                         </div>
                         <div className='text-center'>
--- views/services/new_services.jsx
+++ views/services/new_services.jsx
@@ -7,23 +7,17 @@ function newService () {
             <main id="main-container">
             <h1 style={{textAlign: 'center'}}>Unite Your Pet with People Who Want to Help</h1>
                 <form method='POST' action='/services'>
-                    <div className='form-group row'>
-                        <label className='col-sm-2 col-form-label' htmlFor='service'>Service</label>
-                        <div className='col-sm-10'>
-                            <input className='form-control' required id='service' name='service'/>
-                        </div>
+                    <div className='form-group'>
+                        <label className='col-form-label' htmlFor='service'>Service</label>
+                        <input className='form-control' id='service' name='service' required/>
                     </div>
-                    <div className='form-group row'>
-                        <label className='col-sm-2 col-form-label' htmlFor='name'>Pet Name</label>
-                        <div className='col-sm-10'>
-                            <input className='form-control' required id='name' name='name'/>
-                        </div>
+                    <div className='form-group'>
+                        <label className= 'col-form-label' htmlFor='name'>Pet Name</label>
+                        <input className='form-control' required id='name' name='name'/>
                     </div>
-                    <div className='form-group row'>
-                        <label className='col-form-label col-sm-2' htmlFor='animal_type'>Type of Pet</label>
-                        <div className='col-sm-10'>
+                    <div className='form-group'>
+                        <label className='col-form-label' htmlFor='animal_type'>Type of Pet</label>
                             <select className='form-select' id='animal_type' name='animal_type'>
-                                {/* <option></option> */}
                                 <option>Dog</option>
                                 <option>Cat</option>
                                 <option>Bird</option> 
@@ -32,64 +26,58 @@ function newService () {
                                 <option>Fish</option>
                                 <option>Other</option>
                             </select>
-                        </div>
                     </div>
-                    <div className='form-group row'>
-                        <label className='col-form-label col-sm-2' htmlFor='breed'>Breed</label>
-                        <div className='col-sm-10'>
-                            <input className='form-control' id='breed' name='breed'/>
-                        </div>
+                    <div className='form-group'>
+                        <label className='col-form-label' htmlFor='breed'>Breed</label>
+                        <input className='form-control' id='breed' name='breed'/>
                     </div>
-                    <fieldset className="form-group">
-                        <div className="row">
-                        <legend className="col-form-label col-sm-2 pt-0">Gender</legend>
-                        <div className="col-sm-10">
-                            <div className="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" name="gender" id="gender" value="male" />
-                                <label className="form-check-label" for="gender">
-                                    Male
-                                </label>
-                            </div>
-                            <div className="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" name="gender" id="gender" value="female" />
-                                <label className="form-check-label" for="gender">
-                                    Female
-                                </label>
-                            </div>
-                        </div>
-                        </div>
-                    </fieldset>
-                    <div className='form-group row'>
-                        <label className='col-form-label col-sm-2' htmlFor='age'>Age</label>
-                        <div className='col-sm-5'>
-                            <input className='form-control' type='number' id='age' name='age' min='1' />
-                        </div>
-                        <div className='col-sm-5'>
-                            {/* <label className='col-form-label col-sm-2' htmlFor='age_unit'>Unit</label> */}
-                            <select className='form-select' id='age_unit' name='age_unit'>
-                                <option value='years'>Years</option>
-                                <option value='months'>Months</option>
-                                <option value='weeks'>Weeks</option>
+                    <div className='form-group'>
+                        <label className='col-form-label' htmlFor='gender'>Gender</label>
+                            <select className='form-select' id='gender' name='gender'>
+                                <option>Male</option>
+                                <option>Female</option>
                             </select>
-                        </div>
                     </div>
-                    <div className='form-group row'>
-                        <label className='col-form-label col-sm-2' htmlFor='details'>Details</label>
-                        <div className='col-sm-10'>
-                            <textarea className='form-control' id='details' name='details'/>
+                    {/* <fieldset className="form-group">
+                        <div className="row">
+                            <legend className="col-form-label pt-0">Gender</legend>
+                            <div className="col-sm-10">
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="gender" id="gender" value="male" />
+                                    <label className="form-check-label" for="gender">
+                                        Male
+                                    </label>
+                                </div>
+                                <div className="form-check form-check-inline">
+                                    <input className="form-check-input" type="radio" name="gender" id="gender" value="female" />
+                                    <label className="form-check-label" for="gender">
+                                        Female
+                                    </label>
+                                </div>
+                            </div>
                         </div>
+                    </fieldset> */}
+                    <div className='form-group'>
+                        <label className='col-form-label' htmlFor='age'>Age</label>
+                        <input className='form-control' type='number' id='age' name='age' min='1' />
+                        <label className='col-form-label' htmlFor='age_unit'></label>
+                        <select className='form-select' id='age_unit' name='age_unit'>
+                            <option value='years'>Years</option>
+                            <option value='months'>Months</option>
+                            <option value='weeks'>Weeks</option>
+                        </select>
                     </div>
-                    <div className='form-group row'>
-                        <label className='col-form-label col-sm-2' htmlFor='health_history'>Health History</label>
-                        <div className='col-sm-10'>
-                            <textarea className='form-control' id='health_history' name='health_history'/>
-                        </div>
+                    <div className='form-group'>
+                        <label className='col-form-label' htmlFor='details'>Details</label>
+                        <textarea className='form-control' id='details' name='details'/>
                     </div>
-                    <div className='form-group row'>
-                        <label className='col-form-label col-sm-2' htmlFor='pic'>Picture</label>
-                        <div className='col-sm-10'>
-                            <input className='form-control' id='pic' name='pic'/>
-                        </div>
+                    <div className='form-group'>
+                        <label className='col-form-label' htmlFor='health_history'>Health History</label>
+                        <textarea className='form-control' id='health_history' name='health_history'/>
+                    </div>
+                    <div className='form-group'>
+                        <label className='col-form-label' htmlFor='pic'>Picture</label>
+                        <input className='form-control' id='pic' name='pic'/>
                     </div>
                     <div className='text-center'>
                         <input className='btn btn-primary' type='submit' value='Add Service' />
