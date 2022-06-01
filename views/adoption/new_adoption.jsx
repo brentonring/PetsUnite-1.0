9a20a28258ba3a66e22d7b883fb5fe5d7cafc5ce
Brenton Ring <brentonring@Brentons-Air.lan>
2022-05-31 22:51:23 -0400

    small changes to new adoption

--- views/adoption/new_adoption.jsx
+++ views/adoption/new_adoption.jsx
@@ -8,32 +8,36 @@ function newAdoption () {
                 <h1 style={{textAlign: 'center'}}>Unite a Pet with a Loving Owner</h1>
                 <div>
                     <form method='POST' action='/adoption'>
-                        <div className='form-group'>
-                            <label className= 'col-form-label' htmlFor='name'>Pet Name</label>
-                            <input className='form-control' required id='name' name='name'/>
+                        <div className='row'>
+                            <div className='form-group col-sm-6'>
+                                <label className= 'col-form-label' htmlFor='name'>Pet Name</label>
+                                <input className='form-control' required id='name' name='name'/>
+                            </div>
+                            <div className='form-group col-sm-6'>
+                                <label className='col-form-label' htmlFor='animal_type'>Type of Pet</label>
+                                    <select className='form-select' id='animal_type' name='animal_type'>
+                                        <option>Dog</option>
+                                        <option>Cat</option>
+                                        <option>Bird</option> 
+                                        <option>Rodent</option>
+                                        <option>Reptile</option>
+                                        <option>Fish</option>
+                                        <option>Other</option>
+                                    </select>
+                            </div>
                         </div>
-                        <div className='form-group'>
-                            <label className='col-form-label' htmlFor='animal_type'>Type of Pet</label>
-                                <select className='form-select' id='animal_type' name='animal_type'>
-                                    <option>Dog</option>
-                                    <option>Cat</option>
-                                    <option>Bird</option> 
-                                    <option>Rodent</option>
-                                    <option>Reptile</option>
-                                    <option>Fish</option>
-                                    <option>Other</option>
-                                </select>
-                        </div>
-                        <div className='form-group'>
-                            <label className='col-form-label' htmlFor='breed'>Breed</label>
-                            <input className='form-control' id='breed' name='breed'/>
-                        </div>
-                        <div className='form-group'>
-                            <label className='col-form-label' htmlFor='gender'>Gender</label>
-                                <select className='form-select' id='gender' name='gender' >
-                                    <option>Male</option>
-                                    <option>Female</option>
-                                </select>
+                        <div className='row'>
+                            <div className='form-group col-sm-6'>
+                                <label className='col-form-label' htmlFor='breed'>Breed</label>
+                                <input className='form-control' id='breed' name='breed'/>
+                            </div>
+                            <div className='form-group col-sm-6'>
+                                <label className='col-form-label' htmlFor='gender'>Gender</label>
+                                    <select className='form-select' id='gender' name='gender' >
+                                        <option>Male</option>
+                                        <option>Female</option>
+                                    </select>
+                            </div>
                         </div>
                         <div className='form-group'>
                             <label className='col-form-label' htmlFor='color'>Color</label>
