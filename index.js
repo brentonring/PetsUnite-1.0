550f517607eeeeb8e46514d6d577b589fb9becef
Brenton Ring <97139815+brentonring@users.noreply.github.com>
2023-02-23 08:54:59 -0700

    commented out dotenv so it can preload instead

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Modules and Globals
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const app = express();
 const methodOverride = require("method-override");
